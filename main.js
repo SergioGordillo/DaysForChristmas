28d3b2ada06b3cb35bea6a5103639e19d5ebd6b2
Sergio Gordillo <54623826+SergioGordillo@users.noreply.github.com>
2019-12-19 17:38:36 +0100

    Update main.js
    I have done some changes because I realised there was a mistake with the final days of the year. With this commit, the program will calculate the days before Christmas no matter which day is today, so it's ok :)

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-function calcular(){
+function calcular() {
 
-anio=new Date();
-fechaNavidad=new Date (anio.getFullYear(), 11, 25).getTime(); 
-fechaActual=new Date().getTime(); 
+    var anio = new Date();
+    var fechaNavidad = new Date(anio.getFullYear(), 11, 25).getTime();
+    var fechaActual = new Date().getTime();
 
-var diferenciaDiasMs = fechaNavidad - fechaActual; //Me da la diferencia de días en milisegundos
+    //Con este if/else abarcamos también los días finales del año
 
-var diferenciaDiasDias=(diferenciaDiasMs/(1000*60*60*24));
-                 // (1000*60*60*24) --> milisegundos -> segundos -> minutos -> horas -> días
-                 // con esto transformo la diferencia de días de milisegundos a días
+    if (fechaActual <= fechaNavidad) {
+        var diferenciaDiasMs = fechaNavidad - fechaActual; //Me da la diferencia de días en milisegundos
 
-var diferenciaDiasDiasredondeado=Math.round(diferenciaDiasDias); 
+        var diferenciaDiasDias = (diferenciaDiasMs / (1000 * 60 * 60 * 24));
+        // (1000*60*60*24) --> milisegundos -> segundos -> minutos -> horas -> días
+        // con esto transformo la diferencia de días de milisegundos a días
 
-console.log ("Quedan " + diferenciaDiasDiasredondeado + " días para Navidad"); 
+        var diferenciaDiasDiasredondeado = Math.round(diferenciaDiasDias);
+
+        console.log("Quedan " + diferenciaDiasDiasredondeado + " días para Navidad");
+
+    } else {
+
+        var fechaNavidadAnioQueViene=new Date (anio.getFullYear()+1, 11, 25).getTime();
+        var diferenciaDiasMs = fechaNavidadAnioQueViene - fechaActual; //Me da la diferencia de días en milisegundos
+        var diferenciaDiasDias = (diferenciaDiasMs / (1000 * 60 * 60 * 24));
+        // (1000*60*60*24) --> milisegundos -> segundos -> minutos -> horas -> días
+        // con esto transformo la diferencia de días de milisegundos a días
+
+        var diferenciaDiasDiasredondeado = Math.round(diferenciaDiasDias);
+
+        console.log("Quedan " + diferenciaDiasDiasredondeado + " días para Navidad");
+
+    }
+
 }
